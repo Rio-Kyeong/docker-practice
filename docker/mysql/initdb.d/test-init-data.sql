74a371c3a762026ff01e5fbcd243cb28477eccf2
Leo <drd9811@naver.com>
2023-01-17 16:23:17 +0900

    Docker Compose 설정

---
+++ docker/mysql/initdb.d/test-init-data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into POST values (1, 'post_1');
+
+call next value for hibernate_sequence;
+insert into POST values (2, 'post_2');
+
+call next value for hibernate_sequence;
+insert into POST values (3, 'post_3');
+
+call next value for hibernate_sequence;
+insert into POST values (4, 'post_4');
+
+call next value for hibernate_sequence;
+insert into POST values (5, 'post_5');
