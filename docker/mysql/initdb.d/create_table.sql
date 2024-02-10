2eea0cc1b416ddd60d1a099fb45791e38c9b51a5
Leo <drd9811@naver.com>
2024-02-10 21:09:27 +0900

    DockerFile 추가

--- docker/mysql/initdb.d/create_table.sql
+++ docker/mysql/initdb.d/create_table.sql
@@ -1,7 +1,19 @@
-CREATE TABLE `mysql_db`.`orders` (
-    `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE `mysql_db`.`orders`
+(
+    `id`         INT         NOT NULL AUTO_INCREMENT,
     `order_item` VARCHAR(45) NULL,
-    `price` INT NULL,
-    `order_date` DATE NULL,
+    `price`      INT         NULL,
+    `order_date` DATETIME    NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='주문';
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='주문';
+
+CREATE TABLE `mysql_db`.`post`
+(
+    `id`         binary(16)  NOT NULL COMMENT '게시글 ID',
+    `name`       VARCHAR(45) NULL COMMENT '게시글 명',
+    `created_at` DATETIME    NULL DEFAULT NOW() COMMENT 'DB 데이터 최초 생성 일시',
+    `updated_at` DATETIME    NULL DEFAULT NOW() COMMENT 'DB 데이터 최종 수정 일시',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='게시글';
